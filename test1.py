691be1d0f1599f23290650b07c08979a46bc1ab8
OumaymaDourrou <salamoumayma@gmail.com>
2023-03-29 11:58:44 +0200

    terza modifica

---
+++ test1.py
@@ -0,0 +1,9 @@
+# print('hello world')
+# print('hello world')
+# print('hello world')
+# print('hello world')
+# print('hello world')
+
+from PIL import Image
+img = Image.open('dog.jpg')
+img.show()
