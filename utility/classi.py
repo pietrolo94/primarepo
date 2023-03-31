1e0c7a977c0d7ec02f6392d546295207d3cbd2b6
pietrolo94 <pietrolo.zoffoli1994@gmail.com>
2023-03-31 12:48:34 +0200

    commento

--- utility/classi.py
+++ utility/classi.py
@@ -23,3 +23,24 @@ class Sfera:
         '''calcola la superficie di una sfera'''
         superficie = 4*math.pi*(self.raggio**2)
         return superficie
+
+class Triangolo:
+    def __init__(self, a,b,c):
+        self.cateto1 = a
+        self.cateto2 = b 
+        self.cateto3 = c
+    def perimetro(self):    
+        '''calcola il perimetro del triangolo'''
+        perimetro = self.cateto1+self.cateto2+self.cateto3
+        return perimetro
+    def area(self):
+        '''calcola area del triangolo'''
+        if self.cateto1<(self.cateto2+self.cateto3) and self.cateto2<(self.cateto1+self.cateto3) and self.cateto3<(self.cateto1+self.cateto2):
+            p=self.perimetro()
+            area=math.sqrt(p*(p-self.cateto1)*(p-self.cateto2)*(p-self.cateto3))
+            print("l'area del triangolo è "+str(area))
+            return area
+        else:
+            print("i segmenti inseriti non formano un triangolo")
+
+        
