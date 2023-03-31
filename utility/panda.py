1827ed922be53eb221509e623a0d70360d424df4
pietrolo94 <pietrolo.zoffoli1994@gmail.com>
2023-03-31 15:48:40 +0200

    commento

---
+++ utility/panda.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy.random import rand
+
+d = {'colonna1': [200,300,500,598],
+     'colonna2': [500,660,780,898]
+     }
+
+import pandas as pd
+
+df = pd.DataFrame(d)
+print(df)
